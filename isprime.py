591a584cefdd58c60f8123cae8647bf127ff3f57
krishna0852 <krishnatejapalla1@gmail.com>
2025-02-15 21:33:41 +0530

    added condition to exit if user input is zero

--- isprime.py
+++ isprime.py
@@ -1,14 +1,21 @@
+import sys 
+
 number=int(input("Enter a number to check it's prime or not"))
 
 primeCount=0
 
+if number == 0:
+    #print("Number should not be zero")
+    sys.exit("Number should not be zero")
+
 for i in range(1,number+1):
     if (number % i == 0):
-        print(i)
+        #print(i)
         primeCount=primeCount+1
 
 
 if (primeCount==2):
     print("The number %d is a prime number" %(number))
-else: 
+else:
     print("The number %d is not a prime number" %(number))
+
